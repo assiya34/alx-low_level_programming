8e874e2c99b8ef58bbe607fd0c33b300be000bfa
assiya34 <assiyatarar@gmail.com>
2023-08-12 08:13:07 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,11 @@
  *
  * Description: a C  program that prints with printf function.
  *
- * Return: Always 0 
+ * Return: Always 0
 */
 
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
