9438197ccc5ab6f9d461bf959dbcec85025595c7
assiya34 <assiyatarar@gmail.com>
2023-08-12 14:43:59 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	printf("Size of a chart: %lu byte(s)\n", sizeofa(char));
-	printf("Size of aint: %lu byte(s)\n", sizeofa(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeofa(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeofa(long long int));
-	printf("Size of a float: %lu byte(s)\n", sizeofa(float));
+	printf("Size of a chart: %u byte(s)\n", sizeofa(char));
+	printf("Size of aint: %u byte(s)\n", sizeofa(int));
+	printf("Size of a long int: %u byte(s)\n", sizeofa(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeofa(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeofa(float));
 	return (0);
 }
