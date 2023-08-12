94b7665966aa0e1264ea0408b8d91b5ebfac0e3e
assiya34 <assiyatarar@gmail.com>
2023-08-12 08:34:00 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/***/
+kn
+int main(void)
+{
+	printf("Size of a chart:" ,sizeofa(char));
+	printf("Size of aint:" ,sizeofa(int));
+	printf("Size of a long int" ,sizeofa(long int));
+	printf("Size of a long long int",,sizeofa(long long int));
+	printf("Size of a float" ,sizeofa(float));
+	return (0);
+}
